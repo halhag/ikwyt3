9a0c5027d4e1ed1c9d56a09cf18b1af41a95a2fe
Halvard Hagesæther <halvard.hagesaether@knowit.no>
2020-12-22 18:55:52 +0100

    Additional info

--- Repository/Question.cs
+++ Repository/Question.cs
@@ -5,6 +5,7 @@ namespace Repository
     public class Question
     {
         public string Text { get; set; }
+        public string AdditionalText { get; set; }
         public List<Alternative> Alternatives { get; set; }
         public int Order { get; set; }
     }
